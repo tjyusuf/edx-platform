3922d14c62249c3b98a112c76e6f1195356cc973
Ben Patterson <bpatterson@edx.org>
2016-07-12 18:12:36 -0400

    Additional refactoring based on Cale's feedback.

--- pavelib/assets.py
+++ pavelib/assets.py
@@ -654,6 +654,7 @@ def collect_assets(systems, settings, collectstatic_log="/dev/null"):
     """
 
     # unless specified, collectstatic (which can be very verbose) pipes to /dev/null
+
     if collectstatic_log is None:
         collectstatic_stdout_str = ""
     else:
@@ -779,7 +780,7 @@ def update_assets(args):
         help="list of themes to compile sass for",
     )
     parser.add_argument(
-        '--collect-log', dest='collect_log_file', default=None,
+        '--collect-log', dest='collect_log_file', default="/dev/null",
         help="When running collectstatic, direct output to specified log file",
     )
     args = parser.parse_args(args)
